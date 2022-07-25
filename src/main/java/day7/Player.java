3b000e61fd2dfe9003c4f5b06f3c3158219d56a1
”Nikolay <nregspam@gmail.com>
2022-07-26 01:56:07 +0500

    day 7 is done

--- src/main/java/day7/Player.java
+++ src/main/java/day7/Player.java
@@ -3,28 +3,35 @@ package day7;
 import java.util.Random;
 
 public class Player {
-    private int stamina;
     private static final int MAX_STAMINA = 100;
     private static final int MIN_STAMINA = 0;
     private static int countPlayers = 0;
-
-    public void run(){
-        if (stamina>0){
-            stamina--;
-        }
-    }
-
-    public void info(){
-        if (countPlayers < 6){
-            System.out.println("Команды не полные, осталось " + (6-countPlayers) + " свободных мест.");
-        } else {
-            System.out.println("Свободных мест нет.");
-        }
-
-    }
+    private int stamina;
 
     public Player() {
         Random random = new Random();
         this.stamina = random.nextInt(11) + 90;
+        if (countPlayers < 6) {
+            countPlayers++;
+        }
+    }
+
+    public void run() {
+        if (stamina > MIN_STAMINA) {
+            stamina--;
+            if (stamina == MIN_STAMINA){
+                countPlayers--;
+            }
+        } else {
+            System.out.println("Действие невозможно, игрок выбыл из игры.");
+        }
+    }
+
+    public static void info() {
+        if (countPlayers < 6) {
+            System.out.println("Команды не полные, осталось " + (6 - countPlayers) + " свободных мест.");
+        } else {
+            System.out.println("Свободных мест нет.");
+        }
     }
 }
