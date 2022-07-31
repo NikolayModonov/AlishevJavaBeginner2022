9ac6e752716547c153ea7052f5a8cd433a48d536
”Nikolay <nregspam@gmail.com>
2022-08-01 00:24:30 +0500

    save progress

--- src/main/java/day12/task3/MusicBand.java
+++ src/main/java/day12/task3/MusicBand.java
@@ -1,5 +1,8 @@
 package day12.task3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicBand {
     private String name;
     private int year;
@@ -9,15 +12,29 @@ public class MusicBand {
         this.year = year;
     }
 
-    @Override
-    public String toString() {
-        return "MusicBand {" +
-                "name = '" + name + '\'' +
-                ", year = " + year +
-                '}';
+    public String getName() {
+        return name;
     }
 
     public int getYear() {
         return year;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicBand{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public static List<MusicBand> bandsAfter2000 (List<MusicBand> bands){
+        List<MusicBand> result = new ArrayList<>();
+        for (MusicBand band:bands){
+            if (band.getYear()>1999){
+                result.add(band);
+            }
+        }
+        return result;
     }
 }
