23f9f1cb9b7750fe7a3ad584bce93f9c43e8a09e
”Nikolay <nregspam@gmail.com>
2022-08-01 00:21:39 +0500

    save progress

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,23 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
 
+        list.add("Toy");
+        list.add("Cad");
+        list.add("LAD");
+        list.add("BMW");
+        list.add("Fiat");
+        System.out.println(list);
+
+        list.add(2, "NCR");
+        System.out.println(list);
+
+        list.remove(0);
+        System.out.println(list);
     }
 }
--- src/main/java/day12/task3/MusicBand.java
+++ src/main/java/day12/task3/MusicBand.java
@@ -1,8 +1,5 @@
 package day12.task3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MusicBand {
     private String name;
     private int year;
@@ -12,29 +9,15 @@ public class MusicBand {
         this.year = year;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public String toString() {
+        return "MusicBand {" +
+                "name = '" + name + '\'' +
+                ", year = " + year +
+                '}';
     }
 
     public int getYear() {
         return year;
-    }
-
-    @Override
-    public String toString() {
-        return "MusicBand{" +
-                "name='" + name + '\'' +
-                ", year=" + year +
-                '}';
-    }
-
-    public static List<MusicBand> bandsAfter2000 (List<MusicBand> bands){
-        List<MusicBand> result = new ArrayList<>();
-        for (MusicBand band:bands){
-            if (band.getYear()>1999){
-                result.add(band);
-            }
-        }
-        return result;
     }
 }
